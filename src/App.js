895894fc44eea16fde0bb74b9fcfe8d410fe7648
chathuraed <chathura.dev93@gmail.com>
2024-02-28 12:51:53 +0530

    useState in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/button";
 import Card from "./components/card";
 import Input from "./components/input";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
       <Card />
       <Input
         type="text"
         onChange={(e) => {
-          console.log(e.target.value);
+          setName(e.target.value);
         }}
+        value={name}
       />
       <Button
         onClick={() => {
-          console.log("clicked");
+          console.log("clicked", name);
         }}
         title="Click Me"
       />
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Button = (props) => {
-  console.log(props);
-  return <button onClick={props.onClick}>{props.title}</button>;
+  const { title, onClick } = props;
+  return <button onClick={onClick}>{title}</button>;
 };
 
 export default Button;
